fbda4dd292975da0a1909dbe47590b94f00719b2
greysoh <dev@hofers.cloud>
2023-03-20 14:39:40 -0400

    chore: Adds basic comments describing how TCPish UDP works.

--- libs/internal/TCPishUDP.js
+++ libs/internal/TCPishUDP.js
@@ -9,19 +9,36 @@
 import * as dgram from "https://deno.land/std@0.177.0/node/dgram.ts";
 
 export function createSocketCore(typeOf) {
-  const clientList = [];
-  const evtList = [];
+  const clientList = []; // List of all clients
+  const evtList = []; // List of all event listeners
 
+  // Create the basic server
   const server = dgram.createSocket({
     type: typeOf,
   });
 
   server.on("message", (msg, rinfo) => {
+    // Attempt to find the "client"
+
+    // NOTE:
+    // Client is in quotes because UDP is a connectionless protocol.
+    // Therefore, there is no actual permanent clients.
+    // However, at least on local networks, where pfC is expected to run,
+    // Each client connects with its IP and a port of the device.
+    //
+    // Therefore, we can identify each temporary client based on the above.
+    //
+    // Note that this is not perfect.
+    // We cannot tell if we have disconnected or not, due to a connectionless protocol.
+    // So, when the client is done, we would have no way of realizing,
+    // And could potentially have an entirely new client without realizing.
+
     const client = clientList.find((i) => i.port == rinfo.port && i.address == rinfo.address);
 
     // Check to see if we have "connected" already
+    // Connected is quoted because of the same reasons stated above.
     if (!client) {
-      const clientData = {
+      const clientData = { // Generate a client then push it to the client array
         port: rinfo.port,
         address: rinfo.address,
         evtList: []
@@ -29,7 +46,7 @@ export function createSocketCore(typeOf) {
 
       clientList.push(clientData);
 
-      const filter = evtList.filter((i) => i.type == "connection");
+      const filter = evtList.filter((i) => i.type == "connection"); // Feed it to all connection event listeners
       for (const connEventListener of filter) {
         connEventListener.func({
           on(type, func) {
@@ -47,8 +64,15 @@ export function createSocketCore(typeOf) {
           remotePort: rinfo.port
         });
 
+        // Try to feed any message events registered the message
+        // If you have bad luck, messages could be sent out of order
+        // But, oh well.
+
         function checkIfMsgEventsRegisteredThenDispatch() {
           setTimeout(function() {
+            // Find client because it will get updated
+            // We also need to find the data event listener, for the above reason.
+
             const client = clientList.find((i) => i.port == rinfo.port && i.address == rinfo.address);
             const filter = client.evtList.filter((i) => i.type == "data");
 
@@ -65,8 +89,10 @@ export function createSocketCore(typeOf) {
         checkIfMsgEventsRegisteredThenDispatch();
       }
     } else {
+      // Find the data event listeners for the client
       const filter = client.evtList.filter((i) => i.type == "data");
       
+      // Send all the message data
       for (const msgEventListener of filter) {
         msgEventListener.func(msg);
       }
